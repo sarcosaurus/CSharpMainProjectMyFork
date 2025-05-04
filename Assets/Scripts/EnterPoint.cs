39ce6c412b755a0f7f408506130f3e6da5033847
sarco <sarco.gamedev@gmail.com>
2025-05-04 16:49:39 +0600

    coment

--- Assets/Scripts/EnterPoint.cs
+++ Assets/Scripts/EnterPoint.cs
@@ -12,6 +12,13 @@ public class EnterPoint : MonoBehaviour
     [SerializeField] private Canvas _targetCanvas;
     private float _timeScale = 1;
     
+    // Привет, здесь был я!
+
+    /*/
+    * Привет!
+    *
+    /*/
+
     void Start()
     {
         Time.timeScale = _timeScale;
